24ac7923a5fa38b73490cf0a4a0601ad7b4b2c12
Dimo Stoyanov <stoyanov.dimo@googlemail.com>
2012-11-14 06:29:17 +0100

    script for compilation and execution of whole genotype experiment

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ mkdir "FletcherPowell"
 
 make genospace
 
-for i in {7..8}
+for i in {1..8}
 do
 	radius=$(echo "scale=2; 4 + 0.5 * $i" | bc)
 	
-	for j in {1..2}
+	for j in {1..30}
 	do
 		./genospace 0 $radius
 		mkdir "FletcherPowell/$radius"
